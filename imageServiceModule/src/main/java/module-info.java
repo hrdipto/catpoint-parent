d7920e9fd73a45eaf3ba4532d68d88eb6bcc35df
Habibur Rahman Dipto <jhondipto@gmail.com>
2021-01-15 05:32:46 +0600

    Initial commit

---
+++ imageServiceModule/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.udacity.image {
+    exports com.udacity.image.service;
+
+    requires miglayout;
+    requires java.desktop;
+
+
+}
---
+++ securityServiceModule/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.udacity.security {
+
+
+    requires com.udacity.image;
+    requires miglayout;
+    requires java.desktop;
+
+
+}
